bfcd6d7797968acf5ad2170f6151accad64be17d
Farabi Shohel <66586861+farabi-shohel@users.noreply.github.com>
2022-11-24 02:11:02 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Wow Js Link >>>>>>>>>>>>>>>
+new WOW().init();
+// Wow Js Link >>>>>>>>>>>>>>>
+
+// Counter Up Jquery Plugin=====
+$(document).ready(function(){
+
+	$('.counter').counterUp({
+    delay: 10,
+    time: 1000
+  });
+
+});
